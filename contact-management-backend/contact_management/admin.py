a8f15be67ac12b2bee0c323598d5024a759078ce
Omar Shaikh <syed15-8699@diu.edu.bd>
2024-02-21 15:58:56 +0600

    changed the contact DB

--- contact-management-backend/contact_management/admin.py
+++ contact-management-backend/contact_management/admin.py
@@ -1,6 +1,9 @@
 # register our tables here to show to the admin panel
 
 from django.contrib import admin
-from .models import Contact
+from .models import Contact, ContactProfile, Label, ContactNumber
 
 admin.site.register(Contact)
+admin.site.register(ContactProfile)
+admin.site.register(Label)
+admin.site.register(ContactNumber)
--- contact-management-backend/contact_management/models.py
+++ contact-management-backend/contact_management/models.py
@@ -1,9 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+
 class Contact(models.Model):
     name = models.CharField(max_length=200)
-    description = models.CharField(max_length=500)
+    email = models.CharField(max_length=500)
+    phonenumber = models.CharField(max_length=500)
 
     def __str__(self):
         return self.name+"        "+self.description
 
     # to make the db from this file just make another migration with manage.pt makemigration
+class ContactProfile(models.Model):
+    contact_profile_id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    email = models.EmailField()
+    notes = models.TextField(blank=True, null=True)
+    birthday = models.DateField(blank=True, null=True)
+    company = models.CharField(max_length=255, blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    source = models.TimeField(max_length=255, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+class Label(models.Model):
+    label_id = models.AutoField(primary_key=True)
+    contact_profile = models.ForeignKey(ContactProfile, on_delete=models.CASCADE)
+    label_name = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+class ContactNumber(models.Model):
+    contact_number_id = models.AutoField(primary_key=True)
+    contact_profile = models.ForeignKey(ContactProfile, on_delete=models.CASCADE)
+    number = models.CharField(max_length=20)
+    contact_type = models.CharField(max_length=50)
+    is_primary = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
